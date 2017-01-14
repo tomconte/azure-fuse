e789e7aa10be7a3516cb63aafb5f1fc91e89bc75
Thomas Conte <thomas@conte.com>
2017-01-14 11:04:32 +0100

    Added Dockerfile and docker image build script

---
+++ build_docker.cmd
@@ -0,0 +1,1 @@
+docker run --rm -it --privileged --cap-add=ALL -v /lib/modules:/lib/modules -v C:\dev\FUSE\azure-fuse:/azure-fuse azurefuse:build
--- mount.sh
+++ mount.sh
@@ -2,4 +2,8 @@
 
 # Usage: azurefs <connection_string> <mount_point>
 
+export LD_LIBRARY_PATH=/usr/local/lib
+
+mkdir -p /azure
+
 ./bin/azurefs $1 $2 -f -s
